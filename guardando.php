277039a48d1d0a7d6e2376c804f2da0f4761b146
James Castelo <jamescastelo1107@gmail.com>
2024-03-07 00:31:52 -0300

    Novo commit
    ok

---
+++ guardando.php
@@ -0,0 +1,16 @@
+
+<?php
+require_once ('classes/Pessoa.php');
+
+$nome = $_POST['nome'];
+$idade = $_POST ['idade'];
+
+$pessoa = new Pessoa();
+$pessoa -> setNome ("James");
+$pessoa -> setIdade (28);
+
+echo "<h1>Meu nome é {$pessoa->getNome()} e tenho {$pesssoa-> getIdade()} anos de idade. </h1>";
+
+
+ 
+?>
